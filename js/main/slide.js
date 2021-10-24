c226ecf05e58d3264cea4d45d58361683256c11c
Naoki Kanatani <naokiokane0616@icloud.com>
2021-10-24 13:37:39 +0900

    complete

--- js/main/slide.js
+++ js/main/slide.js
@@ -1,15 +1,18 @@
-// JavaScript Document
-
 'use strict';
 
 $("#index").vegas({ // スライドショーにしたい要素指定
-    slides: [
-        { src: "../images/top/top_1.jpg" }, // 背景画像1枚目
-        { src: "../images/top/top_2.jpg" }, // 背景画像2枚目
-        { src: "../images/top/top_3.jpg"}, // 背景画像3枚目
-    ],
-	animation: 'kenburns',
-	delay: 8000,
-	animationDuration: 9000,
-	transition: 'blur2',
-  });
+  slides: [{
+      src: "images/top/top_1.jpg"
+    }, // 背景画像1枚目
+    {
+      src: "images/top/top_2.jpg"
+    }, // 背景画像2枚目
+    {
+      src: "images/top/top_3.jpg"
+    }, // 背景画像3枚目
+  ],
+  animation: 'kenburns',
+  delay: 8000,
+  animationDuration: 9000,
+  transition: 'blur2',
+});
